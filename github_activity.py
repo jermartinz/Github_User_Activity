33be02f61d2e3d86369bc20f0a3c62a5d4dd00d8
Jeremy A. <61643086+jermartinz@users.noreply.github.com>
2025-09-01 02:53:50 -0400

    Add datetime display and readable CreateEvent output

--- github_activity.py
+++ github_activity.py
@@ -4,6 +4,7 @@ import argparse
 import urllib.request
 import json
 import sys
+from datetime import datetime
 
 def main():
     # Read the username from the terminal
@@ -24,21 +25,28 @@ def main():
     except Exception as e:
         print(f' Error when querying the API: {e}')
         sys.exit(1)
-    
-# Display the last 5 events on screen
-    for event in events[:5]:
-        event_type = event.get('type', 'UnknownEvent')
-        repo_name = event['repo']['name']
-        print(f'{username} did {event_type} in {repo_name}')
-        """
-        if event_type == 'PushEvent':
-            print(f'{username} pushed on {repo_name}')
-        elif event_type == 'IssuesEvent':
-            print(f'{username} opened an issue in {repo_name}')
-        elif event_type == 'WatchEvent':
-            print(f'gave star to {repo_name}')
-        else:
-            print(f'{username} did {event_type} in {repo_name}')
-        """
+
+    if not events:
+        print(f'{username} has no recent activity.')
+    else:    
+    # Display the last 5 events on screen
+        for event in events[:5]:
+            event_type = event.get('type', 'UnknownEvent')
+            repo_name = event['repo']['name']
+            date_event = datetime.strptime(event['created_at'],'%Y-%m-%dT%H:%M:%SZ')
+            date_format = date_event.strftime('%d/%m/%Y %H:%M:%S')
+            if event_type == 'PushEvent':
+                print(f'{date_format} {username} pushed on {repo_name}')
+            elif event_type == 'IssuesEvent':
+                print(f'{date_format} {username} opened an issue in {repo_name}')
+            elif event_type == 'WatchEvent':
+                print(f'{date_format} {username} gave star to {repo_name}')
+            elif event_type == 'CreateEvent':
+                ref_type = event['payload'].get('ref_type', 'something')
+                ref_name = event['payload'].get('ref', '')
+                if ref_name:
+                    print(f'{date_format} {username} created a new {ref_type} {ref_name} in {repo_name}')
+                else:
+                    print(f'{date_format} {username} created a new {ref_type} in {repo_name}')    
 if __name__ == "__main__":
     main()
